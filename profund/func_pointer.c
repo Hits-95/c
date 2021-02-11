bd767c715babbeb0ad6fbfb63b22f89f2aebce10
Ahire Hitesh <hbahire2014@gmail.com>
2021-02-11 16:10:11 +0530

    add function code

---
+++ profund/func_pointer.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+// A normal function with an int parameter
+// and void return type
+int fun(int a, int b)
+{
+    return (a + b);
+}
+
+int main()
+{
+    int (*fun_ptr)(int, int) = fun; //you can remove & also...
+
+    printf("addition id %d ",(fun_ptr)(10,12)); // calling function by ptr *** you can also rermove * like this  (*fun_ptr)(10,12)
+
+    return 0;
+}
