7e8fb53207b06063bfd33497e470ad29ca5414ae
Ahire Hitesh <hbahire2014@gmail.com>
2021-02-11 16:25:21 +0530

    add

--- profund/func_pointer.c
+++ profund/func_pointer.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
-int fun(int a, int b)
+int add(int a, int b)
 {
     return (a + b);
 }
 
 int main()
 {
-    int (*fun_ptr)(int, int) = fun; //you can remove & also...
+    int (*fun_ptr)(int, int) = add; //you can remove & also...
 
     printf("addition id %d ",(fun_ptr)(10,12)); // calling function by ptr *** you can also rermove * like this  (*fun_ptr)(10,12)
 
