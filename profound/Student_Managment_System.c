da0cea279e86702a005415626c73489450a56421
Ahire Hitesh <hbahire2014@gmail.com>
2021-03-04 12:52:49 +0530

    add Puja and Lajari Code

--- profound/Student_Managment_System.c
+++ profound/Student_Managment_System.c
@@ -34,13 +34,164 @@ void add_Employee(){
 	printf("\n\tEnter Department     :\t");
     scanf("%s", emp[i].department);
 	printf("\n\tEnter Salary         :\t");
-    scanf("%s", emp[i].salary);
+    scanf("%f", &emp[i].salary);
 	
 	//increment of i bcoz while adding new employee then its add in new position.
 	i++;
 }
 
+//show the info of all employees
+void displayEmployee(){
+    for(int j = 0; j < i; j++){
+    	printf("\n\tThe Employee Details");
+    	printf("\n\t-------------------------------");
+    	printf("\n\tID         : %d",emp[j].empid);
+    	printf("\n\tFirst Name : %s",emp[j].fname);
+    	printf("\n\tLast Name  : %s",emp[j].lname);
+    	printf("\n\tCompany    : %s",emp[j].company);
+    	printf("\n\tDepartment : %s",emp[j].department);
+    	printf("\n\tSalary     : %f",emp[j].salary);
+    	printf("\n\t-------------------------------");
+    }
+}
+
+//Delete Record of Employee
+void delete_Employee(){
+    int id;
+    printf("\n\tEnter Employee Id to Delete Record.");
+    scanf("%d", &id);
+    
+    for(int j = 0; j <= i; j++){
+        if(id == emp[j].empid){
+            for(int k = j; k < i; k++){
+                emp[k] = emp[k + 1];
+            }
+        }
+    }
+    //decrement i for one record deleted 
+    printf("\n\tOne Record Deleted.");
+    i--;
+}
+
+//Update Employee 
+void update_Employee(){
+    int id, ch;
+    printf("\n\tEnter Employee Id to Update Record :\t");
+    scanf("%d", &id);
+    
+    for(int j = 0; j <= i; j++){
+        if(id == emp[j].empid){
+            printf("\n\tEnter which Record filed you want to update :");
+            printf("\n\t------------------------------------------------");
+            printf("\n\t 1 : First Name.");
+            printf("\n\t 2 : Last Name.");
+            printf("\n\t 3 : Company.");
+            printf("\n\t 4 : Department.");
+            printf("\n\t 5 : Salary.");
+            printf("\n\tEnter Your Choich :\t");
+            scanf("%d",&ch);
+            
+            switch(ch){
+                case 1 : 
+                    printf("\n\tEnter New First Name :\t");
+                    scanf("%s",emp[j].fname);
+                break;
+                case 2 : 
+                    printf("\n\tEnter New Last Name :\t");
+                    scanf("%s",emp[j].lname);
+                break;
+                case 3 : 
+                    printf("\n\tEnter New Company :\t");
+                    scanf("%s",emp[j].company);
+                break;
+                case 4 : 
+                    printf("\n\tEnter New Department :\t");
+                    scanf("%s",emp[j].department);
+                break;
+                case 5 : 
+                    printf("\n\tEnter New Salary :\t");
+                    scanf("%f",&emp[j].salary);
+                break;
+                
+            }
+        }
+    }
+}
+
+//Search Employee
+void search_Employee(){
+    int ch, id;
+    char name[10];
+    
+    printf("\n\tSearch By Name or Id ");
+    printf("\n\t------------------------------------------------");
+    printf("\n\t 1 : Id.");
+    printf("\n\t 2 : First Name.");
+    printf("\n\tEnter Choich :\t");
+    scanf("%d", &ch);
+    switch(ch){
+        case 1 : 
+            printf("\n\tEnter ID of Employee which you want to Search :\t");
+            scanf("%d",id);
+            for(int j = 0; j < i; j++){
+                if(id == emp[j].empid){
+                    printf("\n\tThe Employee Found");
+                	printf("\n\t-------------------------------");
+                	printf("\n\tID         : %d",emp[j].empid);
+                	printf("\n\tFirst Name : %s",emp[j].fname);
+                	printf("\n\tLast Name  : %s",emp[j].lname);
+                	printf("\n\tCompany    : %s",emp[j].company);
+                	printf("\n\tDepartment : %s",emp[j].department);
+                	printf("\n\tSalary     : %f",emp[j].salary);
+                	printf("\n\t-------------------------------");
+                	break;
+                }
+            }
+            printf("\n\tRecord NOT Found.");
+        break ;
+        case 2 : 
+            printf("\n\tEnter First Name of Employee which you want to Search :\t");
+            scanf("%s",name);
+        break ;
+    }
+}
+//main Body
 int main(){
-	add_Employee();
+	int ch;
+	
+	while(1){
+		printf("\n\t 1 : Add Employee.");
+		printf("\n\t 2 : Display Employees.");
+		printf("\n\t 3 : Delete Employee.");
+		printf("\n\t 4 : Update Employee.");
+		printf("\n\t 5 : Search Employee.");
+		printf("\n\t 6 : Exit.");
+
+		printf("\n\tEnter Your Choich :\t");
+		scanf("%d", &ch);
+
+		switch(ch){
+			case 1 : 
+				add_Employee();
+			break;
+			case 2 : 
+				displayEmployee();
+			break;
+			case 3 : 
+				delete_Employee();
+			break;
+			case 4 : 
+				update_Employee();
+			break;
+			case 5 : 
+				search_Employee();
+			break;
+			case 6 :
+			    printf("\n\tThank You !!!");
+			    exit(0);
+			break;
+			default : printf("\n\tInvalide Choich !!!");
+		}
+	}
 	return 0;
 }
